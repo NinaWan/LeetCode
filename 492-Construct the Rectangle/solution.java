8a3723e7527a462fb852814e3cbc1a2ea47c4965
Nina Wan <aileenwnn@gmail.com>
2017-05-11 11:49:24 -0400

    05/10/2017 First Commit

---
+++ 492-Construct the Rectangle/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] constructRectangle(int area) {
+        if(area == 0) return new int[2];
+        int[] result = new int[2];
+        for(int w = 1; w <= area; w++){
+            if(area%w != 0) continue;
+            if(w <= area/w){
+                result[1] = w;
+                result[0] = area/w;
+            }else{
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ 563-Binary Tree Tilt/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int findTilt(TreeNode root) {
+        if(null == root) return 0;
+        return findTilt(root.left) + findTilt(root.right) + Math.abs(subtreeSum(root.left) - subtreeSum(root.right));
+    }
+
+    private int subtreeSum(TreeNode root){
+        if(null == root) return 0;
+        return subtreeSum(root.left)+subtreeSum(root.right)+root.val;
+    }
+}
