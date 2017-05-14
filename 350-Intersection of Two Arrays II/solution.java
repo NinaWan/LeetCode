cabc3d52690a1152747877dca0d93c2a84b98b00
Nina Wan <aileenwnn@gmail.com>
2017-05-14 19:09:25 -0400

    05/12/2017 First Commit

---
+++ 350-Intersection of Two Arrays II/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> freq = new HashMap();
+        List<Integer> intersection = new ArrayList();
+
+        for(int num1:nums1){
+            freq.put(num1, freq.getOrDefault(num1, 0)+1);
+        }
+        for(int num2:nums2){
+            if(freq.getOrDefault(num2, 0) != 0){
+                intersection.add(num2);
+                freq.put(num2, freq.get(num2)-1);
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        for(int i=0; i<result.length; i++) result[i] = intersection.get(i);
+        return result;
+    }
+}
---
+++ 401-Binary Watch/solution.java
@@ -0,0 +1,54 @@
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> result = new ArrayList();
+
+        if(num < 11){
+            for(int i=0; i<= (num>4?4:num); i++){
+                List<String> hours = new ArrayList();
+                List<String> mins = new ArrayList();
+
+                generateHours(i, hours, 0, 0);
+                generateMins(num-i, mins, 0, 0);
+
+                for(String hour:hours){
+                    for(String min:mins){
+                        if(Integer.valueOf(hour)>11 || Integer.valueOf(min)>59) continue;
+                        result.add(hour+":"+min);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void generateHours(int num, List<String> hours, int start, int sum){
+        if(num == 0){
+            hours.add(String.valueOf(sum));
+            return;
+        }
+
+        // Backtracking
+        for(int i=start; i<4; i++){
+            generateHours(num-1, hours, i+1, sum+(int)Math.pow(2,i));
+        }
+
+    }
+
+    private void generateMins(int num, List<String> mins, int start, int sum){
+        if(num == 0){
+            if(sum<10){
+                mins.add("0"+String.valueOf(sum));
+            }else{
+                mins.add(String.valueOf(sum));
+            }
+            return;
+        }
+
+        // Backtracking
+        for(int i=start; i<6; i++){
+            generateMins(num-1, mins, i+1, sum+(int)Math.pow(2,i));
+        }
+
+    }
+}
