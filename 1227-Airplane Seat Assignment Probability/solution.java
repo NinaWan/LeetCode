aa1f7051fdf6c21a7da88374e519ce7b16efa32f
Nina Wan <aileenwnn@gmail.com>
2020-10-03 22:13:31 +0800

    Add solutions for 1227

--- 1227-Airplane Seat Assignment Probability/solution.java
+++ 1227-Airplane Seat Assignment Probability/solution.java
@@ -1,5 +1,5 @@
 class Solution {
     public double nthPersonGetsNthSeat(int n) {
-        return n == 1 ? 1 : 1.0 / n + ((double) (n - 2) / n) * nthPersonGetsNthSeat(n - 1);
+        return n == 1 ? 1 : 0.5;
     }
 }
