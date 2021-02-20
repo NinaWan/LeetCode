0defbec5016ca2ffab929118c65e8f1e95a7fc7a
Nina Wan <aileenwnn@gmail.com>
2021-02-20 10:03:44 +0800

    Add solutions for 1283

--- 1283-Find the Smallest Divisor Given a Threshold/solution.java
+++ 1283-Find the Smallest Divisor Given a Threshold/solution.java
@@ -1,18 +1,17 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int start = 1;
-        int end = Arrays.stream(nums).max().getAsInt();
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            int sum = getDivisionSum(nums, mid);
-            if (sum > threshold) {
-                start = mid + 1;
+        int low = 1, high = (int) 1e6;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (getDivisionSum(nums, mid) <= threshold) {
+                high = mid;
             } else {
-                end = mid;
+                low = mid + 1;
             }
         }
 
-        return start;
+        return low;
     }
 
     private int getDivisionSum(int[] nums, int divisor) {
